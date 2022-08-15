3482f4007b2fec516caff056396d901e63746efa
sujit1984 <sujitsugathan2002@gmail.com>
2022-08-15 05:31:32 +0530

    Updates to the TestBrowser.java file
    Changes made in the TestBrowser.java file to incorporate the automatic downloads of drivers using WebDriverManager class and updates made to the pom.xml for the required dependencies

---
+++ SeleniumTest/src/test/java/testcases/TestBrowser.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//import
+public class TestBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+	
+
+}
+}
