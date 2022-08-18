727c352f5a121d4e52e7db22107a4c15d25217f2
sujit1984 <sujitsugathan2002@gmail.com>
2022-08-18 21:32:34 +0530

    Changes made to connect to the SWAG Labs website
    Added the selenium login to the SWAG Labs and validating the webpage title

---
+++ SeleniumTest/src/test/java/testcases/TestWebElements.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestWebElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		
+		System.out.println(driver.getTitle());
+		
+		//driver.close();
+
+	}
+
+}
